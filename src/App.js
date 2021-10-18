3f07189ddabcc7d27cce754fbda7adc4378004ad
jameelgusau <jameelgusau@gmail.com>
2021-10-18 14:44:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+const App = (props) =>{
+    const { name } = props;
+    return (
+      <>
+        <h1 className="h1">
+          Hello {name}
+        </h1>
+      </>
+    );
+}
+
+export default hot(App);
